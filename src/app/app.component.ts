e774d1339b16f5393036dd1d6f26d0cf8e7386c4
akain88 <larahaze88@gmail.com>
2021-04-28 22:48:06 -0500

    added on hit events and interaction between father-child, child-father-child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { Page2Component } from './page2/page2.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SimpleAngularApp';
+
+  @ViewChild('footer', {static: true})
+  footerComponent: FooterComponent;
+
+  @ViewChild('page2', {static: true})
+  page2Component: Page2Component;
+
+  startTime: String;
+
+  currentPage = 1;
+
+  updatedLastAccessed(){
+    console.log('the previous last accessed value was' + this.footerComponent.lastAccessed)
+    this.footerComponent.lastAccessed = new Date().toString();
+
+  }
+
+  ngOnInit(): void {
+    this.startTime = Date().toString();
+  }
+
+  incrementHitCounter(page){
+    this.currentPage = page;
+    if (page === 2){
+    this.page2Component.incrementHitCounter();
+    }
+
+  }
 }
